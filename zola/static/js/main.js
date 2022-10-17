9430bd650ae92bb7957009fcdc7f0d659ab2dc18
Patryk Palej <patrykpalej@gmail.com>
2022-10-17 22:11:43 +0200

    Just another update

--- zola/static/js/main.js
+++ zola/static/js/main.js
@@ -26,7 +26,7 @@ document.getElementById("mode").addEventListener("click", () => {
 
 // -----
 contactImage = document.getElementById("contact")
-if localStorage.getItem("theme") == "light" {
+if (localStorage.getItem("theme") == "light") {
 	contactImage.src = "https://cloud.patrykpalej.com/index.php/apps/files_sharing/publicpreview/jKzSxxNdsEMwBTQ?x=1908&y=661&a=true&file=email_lm.png&scalingup=0"
 } else {
 	contactImage.src = "https://cloud.patrykpalej.com/index.php/apps/files_sharing/publicpreview/tFgPoebteaf3GLT?x=1908&y=661&a=true&file=email.png&scalingup=0"
